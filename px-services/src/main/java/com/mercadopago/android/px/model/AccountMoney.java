28ffe4c4d6d3c25a4125b89df09d4fa3a5ba41d8
valeserber <valserber@gmail.com>
2018-11-29 15:11:31 -0300

    deleted token

--- px-services/src/main/java/com/mercadopago/android/px/model/AccountMoney.java
+++ px-services/src/main/java/com/mercadopago/android/px/model/AccountMoney.java
@@ -1,5 +1,6 @@
 package com.mercadopago.android.px.model;
 
+import android.support.annotation.NonNull;
 import java.math.BigDecimal;
 
 public class AccountMoney {
@@ -7,16 +8,13 @@ public class AccountMoney {
     private PaymentMethod paymentMethod;
     private BigDecimal availableBalance;
     private boolean invested;
-    private String token;
 
-    public String getToken() {
-        return token;
-    }
-
+    @NonNull
     public PaymentMethod getPaymentMethod() {
         return paymentMethod;
     }
 
+    @NonNull
     public BigDecimal getAvailableBalance() {
         return availableBalance;
     }
