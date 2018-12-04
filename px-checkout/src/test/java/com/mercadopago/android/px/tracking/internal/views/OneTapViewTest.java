1059765ef07c39629cc8c49241bd1820c1c58fa7
Lucas Emmanuel Bais <lucas.bais@mercadolibre.com>
2018-12-04 09:59:25 -0300

    added listener tests

--- px-checkout/src/test/java/com/mercadopago/android/px/tracking/internal/views/OneTapViewTest.java
+++ px-checkout/src/test/java/com/mercadopago/android/px/tracking/internal/views/OneTapViewTest.java
@@ -1,11 +1,18 @@
 package com.mercadopago.android.px.tracking.internal.views;
 
+import android.support.annotation.NonNull;
+import com.mercadopago.android.px.tracking.PXTracker;
+import com.mercadopago.android.px.tracking.PXTrackingListener;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class OneTapViewTest {
@@ -16,5 +23,26 @@ public class OneTapViewTest {
     public void verifyPath() {
         assertEquals(EXPECTED_PATH, new OneTapView(Collections.EMPTY_LIST, null,
             null, null, Collections.EMPTY_LIST).getViewPath());
+    }
+
+    @Test
+    public void verifyListenerCalled() {
+        final PXTrackingListener listener = mock(PXTrackingListener.class);
+        PXTracker.setListener(listener);
+        final Map<String, Object> data = emptyOneTapData();
+        new OneTapView(Collections.EMPTY_LIST, null, null, null, Collections.EMPTY_LIST).track();
+        verify(listener).onView(EXPECTED_PATH, data);
+    }
+
+    @NonNull
+    private Map<String, Object> emptyOneTapData() {
+        final Map<String, Object> data = new HashMap<>();
+        data.put("total_amount", null);
+        data.put("discount", null);
+        data.put("available_methods", Collections.EMPTY_LIST);
+        data.put("items", Collections.EMPTY_LIST);
+        data.put("flow", null);
+        data.put("flow_detail", Collections.EMPTY_MAP);
+        return data;
     }
 }
