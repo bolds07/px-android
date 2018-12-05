692c7e91ed34099801864dd1831936cbc33d4b17
Lucas Emmanuel Bais <lucas.bais@mercadolibre.com>
2018-12-05 13:55:04 -0300

    modified installments tracking info

---
+++ px-checkout/src/main/java/com/mercadopago/android/px/tracking/internal/model/PayerCostInfoList.java
@@ -0,0 +1,18 @@
+package com.mercadopago.android.px.tracking.internal.model;
+
+import android.support.annotation.NonNull;
+import com.mercadopago.android.px.model.PayerCost;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PayerCostInfoList extends TrackingMapModel {
+
+    private final List<PayerCostInfo> availableInstallments;
+
+    public PayerCostInfoList(@NonNull final List<PayerCost> payerCosts) {
+        availableInstallments = new ArrayList<>();
+        for (final PayerCost payerCost : payerCosts) {
+            availableInstallments.add(new PayerCostInfo(payerCost));
+        }
+    }
+}
