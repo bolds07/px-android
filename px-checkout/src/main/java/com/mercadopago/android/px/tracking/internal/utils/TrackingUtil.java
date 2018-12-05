a3770d66c71286129497f56f220c4ea90a9d35b0
Lucas Emmanuel Bais <lucas.bais@mercadolibre.com>
2018-12-05 16:45:50 -0300

    removed unused constants

--- px-checkout/src/main/java/com/mercadopago/android/px/tracking/internal/utils/TrackingUtil.java
+++ px-checkout/src/main/java/com/mercadopago/android/px/tracking/internal/utils/TrackingUtil.java
@@ -45,17 +45,10 @@ public final class TrackingUtil {
     //Additional Info Keys
     public static final String PROPERTY_PAYMENT_METHOD_ID = "payment_method_id";
     public static final String PROPERTY_PAYMENT_TYPE_ID = "payment_method_type";
-    public static final String PROPERTY_ISSUER_ID = "issuer_id";
-    public static final String PROPERTY_PAYMENT_STATUS = "payment_status";
-    public static final String PROPERTY_PAYMENT_ID = "payment_id";
-    public static final String PROPERTY_PAYMENT_STATUS_DETAIL = "payment_status_detail";
-    public static final String PROPERTY_PAYMENT_IS_EXPRESS = "is_express";
     public static final String PROPERTY_ERROR_STATUS = "error_status";
     public static final String PROPERTY_ERROR_CODE = "error_code";
     public static final String PROPERTY_ERROR_REQUEST = "error_request_origin";
     public static final String PROPERTY_ERROR_MESSAGE = "error_message";
-
-    public static final String IS_EXPRESS_DEFAULT_VALUE = "false";
 
     private TrackingUtil() {
     }
