f3f0e914c0c3748c4b108cb2bd8fd94deb5a07b6
matiasromar <matias.romar@mercadolibre.com>
2018-12-07 16:50:13 -0300

    renamed flow by product id

--- px-checkout/src/main/java/com/mercadopago/android/px/internal/datasource/GroupsService.java
+++ px-checkout/src/main/java/com/mercadopago/android/px/internal/datasource/GroupsService.java
@@ -123,7 +123,7 @@ public class GroupsService implements GroupsRepository {
             .setPrivateKey(paymentSettingRepository.getPrivateKey())
             .setPayerEmail(paymentSettingRepository.getCheckoutPreference().getPayer().getEmail())
             .setMarketplace(checkoutPreference.getMarketplace())
-            .setFlow(discountParamsConfiguration.getFlow())
+            .setFlow(discountParamsConfiguration.getProductId())
             .setLabels(discountParamsConfiguration.getLabels()).build();
 
         return checkoutService
